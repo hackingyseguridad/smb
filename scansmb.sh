a8b744c95cd06d6f465aad573f9a1e1e0e6437f7
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-22 14:59:42 +0100

    Update scansmb.sh

--- scansmb.sh
+++ scansmb.sh
@@ -2,6 +2,6 @@
 # simple script para escanear el servicioSamba SMB versiñin 1, en Bash Shell 1.0.x, con nmap
 # uso.:  $./escansbm.sh  IP/rango 
 # (r) hackingyseguridad.com 2026
-# @antonio_taboada
+# https://nmap.org/nsedoc/scripts/smb-vuln-ms17-010.html
 
 nmap -Pn -sVC -p 139,445 --open -O  $1 $2 --script smb-vuln-ms17-010 
