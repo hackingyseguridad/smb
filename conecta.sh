414c2759c4b1d57cad1ab39206ec1aa6d03b7df8
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-22 14:03:01 +0100

    Update conecta.sh

--- conecta.sh
+++ conecta.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
 
-# SMBv1
+# Conexion manual a SMB versiñin 1, en Bash Shell 1.0.x
+# Si conecta, se confirma que es SMBv1 !!! 
+# (r) hackingyseguridad.com 2026
+# @antonio_taboada
+
+# SMBv1 comando
 smbclient -L //$1/ -m NT1
