5f2ee4df011d7fc59543fe66dc7d4309064fd04a
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-22 14:04:45 +0100

    Update ethernalblue.sh

--- ethernalblue.sh
+++ ethernalblue.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 
+# Exploit para SMB versiñin 1, en Bash Shell 1.0.x, con Metaexploit
+# uso.:  $./ethernalblue.sh  IP/fqdn 
+# (r) hackingyseguridad.com 2026
+# @antonio_taboada
 
 msfconsole -q -x "use auxiliary/scanner/smb/smb_ms17_010; set RHOSTS $1; run; exit"
