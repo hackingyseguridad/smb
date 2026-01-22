d58b342cca6fe3abe029816b9923117f11aec9b7
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-22 13:50:22 +0100

    Create scanvuln.sh

---
+++ scanvuln.sh
@@ -0,0 +1,13 @@
+# Scan single host
+# ./scanvuln.sh -t 192.168.1.100
+
+# Scan with verbose output
+#./scanvuln.sh -t 192.168.1.100 -v
+
+# Scan a range
+# ./scanvuln.sh -r 192.168.1.0/24 -o results.txt
+
+# Use different SMB port
+# ./scanvuln.sh -t 192.168.1.100 -p 139
+
+
